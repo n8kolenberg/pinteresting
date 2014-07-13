5aa51c85284a2df5128b47b732bf252d31ca8f5b
n8kolenberg <nkolenberg@gmail.com>
2014-07-13 17:08:04 +0100

    I think I fixed localhost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713191244) do
+ActiveRecord::Schema.define(version: 20140713160615) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140713191244) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
